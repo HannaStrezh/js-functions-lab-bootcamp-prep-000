aea7a88166c509f83d4bf76695769b3254b5b2ce
Hanna Strezh <hannastrezh@gmail.com>
2018-07-02 15:42:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,3 @@ function happyHolidays(){
 function happyHolidaysTo(name) {
   return "Happy holidays, you!"
 }
-function happyHolidayTo(holiday, name) {
-  return "Happy ${holiday}, you!"
-}
-function holidayCountdown('holiday', 'days') {
-  return "It's ${days} days until ${holiday}!"
-}

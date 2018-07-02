fe326ce75591b973ca267ed5fbbb7687551cf2c9
Hanna Strezh <hannastrezh@gmail.com>
2018-07-02 15:06:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-function happyHolidaysTo (name) {
+function happyHolidaysTo (string) {
   return "Happy holidays, " + `${name}` + "!";	
 }
  	 
- function happyHolidayTo (holiday, name) {	 function happyHolidayTo (holiday, name) {
--  return "Happy " + `${holiday}` + ", " + `${name}` + "!";	+  return `Happy ${holiday}, ${name}!`;
- }	 }
- 	 
- function holidayCountdown (holiday, days) {	 function holidayCountdown (holiday, days) {
--  return "It's " + `${days}` + " days until " + `${holiday}` + "!";	+  return `It's ${days} days until ${holiday}!`;
- }// write your code below!
+ 

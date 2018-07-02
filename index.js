c30cb588cb7eaafd035d276a0187cea63804ba83
Hanna Strezh <hannastrezh@gmail.com>
2018-07-02 15:32:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ function happyHolidays(){
   return "Happy holidays!";
 }
  happyHolidays()
- function happyHolidaysTo(greeting, name){
-   return `Happy holidays, ${name}!`
+ function happyHolidaysTo(name){
+   console.log(`Happy holidays, ${name}!`)
  }
  happyHolidaysTo()
+function sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}!`)
+}
